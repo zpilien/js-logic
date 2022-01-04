b8dbc852c6a9199a2a98d3798caa598f573d3299
vic-lee <96267768+vic-lee@users.noreply.github.com>
2022-01-03 21:34:47 -1000

    Update exercises.js

--- exercises.js
+++ exercises.js
@@ -1,1 +1,92 @@
 
+/*Conditional statements gives us the ability to check conditions and change the behavior of the program accordingly:
+if 
+else if
+else 
+*/
+
+//Invoke your functions and console.log your results
+
+//1. Create a function named isRainy which takes a boolean parameter boo. This function will return the statement 'Bring an umbrella' if the boolean value is true.
+
+
+
+/*2. Create a function named vaccinated which takes a boolean parameter boo. 
+If the value is true, it'll return the statement: 'Please come in.' 
+If the value is false, it'll return the statement: 'Please show me your Covid test.'
+*/
+
+
+
+
+/*3. Create a function named scrabble which takes two string parameters word1, word2. 
+If word1 is longer than word2, it will return the statement: 'premium score'. 
+If not, it will return 'normal score'.
+*/
+
+
+
+
+/*4. Create a function named shopping which takes one number parameter budget. 
+If the budget is over 100, return 'Banana Republic';
+If the budget is over 50, return 'Gap';
+If the budget is 50 or less, return 'Old Navy';
+*/
+
+
+
+/*5. Create a function named bouncer which takes two number parameters, age, cover.
+If the age is greater than or equal to 21 AND if the cover is greater than or equal to 10, return 'Please enter'
+If not, return 'Sorry buddy'
+*/
+
+
+
+/*6. Create a function named even which takes a number parameter num.
+If the number is even, it'll return true. 
+If not, it'll return false.
+*/
+
+
+
+
+//Loops allow you to do the same action(s) on every item in a list
+
+//1. Create a for loop that will print number 0 - 10
+
+
+
+//2. Create a for loop that will iterate and print each element in the array below.
+
+let cookies = ['peanut butter', 'chocolate chip', 'almond', 'oreos', 'lemon'];
+
+/*3. Create a for loop that will iterate each element in the array and print the following:
+Player 0 is Tatum.
+Player 1 is Hardaway.
+Player 2 is Leonard.
+Player 3 is Paul.
+Player 4 is Webber.
+Player 5 is Kidd.
+*/
+
+let players = ['Tatum', 'Hardaway', 'Leonard', 'Paul', 'Webber', 'Kidd'];
+
+//4. Create a for loop that will push 20 odd numbers to the oddNum array. Start from 1. 
+
+let oddNum = [];
+
+
+
+
+//5. Create a for loop that will push all even indexed rappers to the eastSide array and odd indexed rappers to the westSide array.
+
+let rappers = ['DMX', 'Snoop', 'Jay-Z', 'Dre', 'Nas', 'Tupac', 'Biggie', 'Ice Cube', 'Ghostface Killah', 'Nipsey Hussle'];
+let eastSide = [];
+let westSide = [];
+
+
+
+//6. Create a for loop that will sum up numbers from 1 to 100 and store the value in the sum variable.
+
+let sum = 0;
+
