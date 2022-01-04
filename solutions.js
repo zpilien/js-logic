ff90086ba9957f1605d7bf031fae3e9e45809946
vic-lee <96267768+vic-lee@users.noreply.github.com>
2022-01-03 20:08:18 -1000

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -1,1 +1,15 @@
 
+/*Conditional statements gives us the ability to check conditions and change the behavior of the program accordingly:
+if 
+else if
+else 
+*/
+
+//1. Create a function named isRainy which takes a boolean parameter boo. This function will return the statement 'Bring an umbrella' if the boolean value is true.
+
+function isRainy(boo){
+  if(boo === true){
+    return 'bring an umbrella';
+  };
+};
+console.log(isRainy(true));
